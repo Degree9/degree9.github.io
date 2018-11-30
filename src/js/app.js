fc7b97b1889e02bfb16d27e27654851a30418ee7
Matthew Ratzke <matt@degree9.io>
2018-11-29 18:04:11 -0700

    gulp js

--- src/js/app.js
+++ src/js/app.js
@@ -53,7 +53,7 @@ document.addEventListener("DOMContentLoaded", function () {
       "line_linked": {
         "enable": true,
         "distance": 150,
-        "color": "#ffffff",
+        "color": "#000000",
         "opacity": 0.4,
         "width": 1
       },
